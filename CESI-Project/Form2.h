23607328d88d70f1904df4116464c8cdb9de5411
Jebril ⚡ <hocine.jebril@orange.fr>
2022-11-28 14:10:07 +0100

    edit Form2

--- CESI-Project/Form2.h
+++ CESI-Project/Form2.h
@@ -34,12 +34,23 @@ namespace CESIProject {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Panel^ panelMenu;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Panel^ panelLogo;
+	private: System::Windows::Forms::PictureBox^ btnHome;
+	private: System::Windows::Forms::FlowLayoutPanel^ panelTitleBar;
 	protected:
-	private: System::Windows::Forms::Panel^ panel2;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Button^ BtnDashboard;
+
+	protected:
+
+
+
+
 
 
 	protected:
@@ -58,91 +69,190 @@ namespace CESIProject {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Form2::typeid));
-			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->panel2 = (gcnew System::Windows::Forms::Panel());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->BtnDashboard = (gcnew System::Windows::Forms::Button());
-			this->panel1->SuspendLayout();
-			this->panel2->SuspendLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->panelMenu = (gcnew System::Windows::Forms::Panel());
+			this->panelLogo = (gcnew System::Windows::Forms::Panel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->btnHome = (gcnew System::Windows::Forms::PictureBox());
+			this->panelTitleBar = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->panelMenu->SuspendLayout();
+			this->panelLogo->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->btnHome))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// panel1
+			// panelMenu
 			// 
-			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(24)), static_cast<System::Int32>(static_cast<System::Byte>(30)),
-				static_cast<System::Int32>(static_cast<System::Byte>(54)));
-			this->panel1->Controls->Add(this->BtnDashboard);
-			this->panel1->Controls->Add(this->panel2);
-			this->panel1->Location = System::Drawing::Point(0, 0);
-			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(213, 677);
-			this->panel1->TabIndex = 0;
+			this->panelMenu->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(31)), static_cast<System::Int32>(static_cast<System::Byte>(30)),
+				static_cast<System::Int32>(static_cast<System::Byte>(68)));
+			this->panelMenu->Controls->Add(this->button6);
+			this->panelMenu->Controls->Add(this->button5);
+			this->panelMenu->Controls->Add(this->button4);
+			this->panelMenu->Controls->Add(this->button3);
+			this->panelMenu->Controls->Add(this->button2);
+			this->panelMenu->Controls->Add(this->button1);
+			this->panelMenu->Controls->Add(this->panelLogo);
+			this->panelMenu->Dock = System::Windows::Forms::DockStyle::Left;
+			this->panelMenu->Location = System::Drawing::Point(0, 0);
+			this->panelMenu->Name = L"panelMenu";
+			this->panelMenu->Size = System::Drawing::Size(220, 673);
+			this->panelMenu->TabIndex = 0;
 			// 
-			// panel2
+			// panelLogo
 			// 
-			this->panel2->Controls->Add(this->label1);
-			this->panel2->Controls->Add(this->pictureBox1);
-			this->panel2->Location = System::Drawing::Point(0, 1);
-			this->panel2->Name = L"panel2";
-			this->panel2->Size = System::Drawing::Size(213, 153);
-			this->panel2->TabIndex = 0;
+			this->panelLogo->Controls->Add(this->btnHome);
+			this->panelLogo->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panelLogo->Location = System::Drawing::Point(0, 0);
+			this->panelLogo->Name = L"panelLogo";
+			this->panelLogo->Size = System::Drawing::Size(220, 140);
+			this->panelLogo->TabIndex = 0;
 			// 
-			// pictureBox1
+			// button1
 			// 
-			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(74, 24);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(56, 57);
-			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::CenterImage;
-			this->pictureBox1->TabIndex = 1;
-			this->pictureBox1->TabStop = false;
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->button1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Century Gothic", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(126)),
-				static_cast<System::Int32>(static_cast<System::Byte>(249)));
-			this->label1->Location = System::Drawing::Point(71, 93);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(61, 20);
-			this->label1->TabIndex = 2;
-			this->label1->Text = L"Admin";
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.Image")));
+			this->button1->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button1->Location = System::Drawing::Point(0, 140);
+			this->button1->Name = L"button1";
+			this->button1->Padding = System::Windows::Forms::Padding(10, 0, 20, 0);
+			this->button1->Size = System::Drawing::Size(220, 60);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Dashboard";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
-			// BtnDashboard
+			// button2
 			// 
-			this->BtnDashboard->FlatAppearance->BorderSize = 0;
-			this->BtnDashboard->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->BtnDashboard->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->button2->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button2->FlatAppearance->BorderSize = 0;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Century Gothic", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->BtnDashboard->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(126)),
-				static_cast<System::Int32>(static_cast<System::Byte>(249)));
-			this->BtnDashboard->Location = System::Drawing::Point(0, 160);
-			this->BtnDashboard->Name = L"BtnDashboard";
-			this->BtnDashboard->Size = System::Drawing::Size(210, 42);
-			this->BtnDashboard->TabIndex = 1;
-			this->BtnDashboard->Text = L"Dashboard";
-			this->BtnDashboard->UseVisualStyleBackColor = true;
+			this->button2->ForeColor = System::Drawing::Color::White;
+			this->button2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button2.Image")));
+			this->button2->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button2->Location = System::Drawing::Point(0, 200);
+			this->button2->Name = L"button2";
+			this->button2->Padding = System::Windows::Forms::Padding(10, 0, 20, 0);
+			this->button2->Size = System::Drawing::Size(220, 60);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Orders";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Century Gothic", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->ForeColor = System::Drawing::Color::White;
+			this->button3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button3.Image")));
+			this->button3->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button3->Location = System::Drawing::Point(0, 260);
+			this->button3->Name = L"button3";
+			this->button3->Padding = System::Windows::Forms::Padding(10, 0, 20, 0);
+			this->button3->Size = System::Drawing::Size(220, 60);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Produts";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button4->FlatAppearance->BorderSize = 0;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Font = (gcnew System::Drawing::Font(L"Century Gothic", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button4->ForeColor = System::Drawing::Color::White;
+			this->button4->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button4.Image")));
+			this->button4->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button4->Location = System::Drawing::Point(0, 320);
+			this->button4->Name = L"button4";
+			this->button4->Padding = System::Windows::Forms::Padding(10, 0, 20, 0);
+			this->button4->Size = System::Drawing::Size(220, 60);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"Customers";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button5->FlatAppearance->BorderSize = 0;
+			this->button5->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button5->Font = (gcnew System::Drawing::Font(L"Century Gothic", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button5->ForeColor = System::Drawing::Color::White;
+			this->button5->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button5.Image")));
+			this->button5->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button5->Location = System::Drawing::Point(0, 380);
+			this->button5->Name = L"button5";
+			this->button5->Padding = System::Windows::Forms::Padding(10, 0, 20, 0);
+			this->button5->Size = System::Drawing::Size(220, 60);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"Marketing";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button6->FlatAppearance->BorderSize = 0;
+			this->button6->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button6->Font = (gcnew System::Drawing::Font(L"Century Gothic", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button6->ForeColor = System::Drawing::Color::White;
+			this->button6->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button6.Image")));
+			this->button6->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button6->Location = System::Drawing::Point(0, 440);
+			this->button6->Name = L"button6";
+			this->button6->Padding = System::Windows::Forms::Padding(10, 0, 20, 0);
+			this->button6->Size = System::Drawing::Size(220, 60);
+			this->button6->TabIndex = 5;
+			this->button6->Text = L"Settings";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// btnHome
+			// 
+			this->btnHome->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"btnHome.Image")));
+			this->btnHome->Location = System::Drawing::Point(0, 1);
+			this->btnHome->Name = L"btnHome";
+			this->btnHome->Size = System::Drawing::Size(220, 139);
+			this->btnHome->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->btnHome->TabIndex = 0;
+			this->btnHome->TabStop = false;
+			// 
+			// panelTitleBar
+			// 
+			this->panelTitleBar->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(26)), static_cast<System::Int32>(static_cast<System::Byte>(25)),
+				static_cast<System::Int32>(static_cast<System::Byte>(62)));
+			this->panelTitleBar->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panelTitleBar->Location = System::Drawing::Point(220, 0);
+			this->panelTitleBar->Name = L"panelTitleBar";
+			this->panelTitleBar->Size = System::Drawing::Size(962, 75);
+			this->panelTitleBar->TabIndex = 1;
 			// 
 			// Form2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(46)), static_cast<System::Int32>(static_cast<System::Byte>(51)),
-				static_cast<System::Int32>(static_cast<System::Byte>(73)));
+			this->BackColor = System::Drawing::Color::White;
 			this->ClientSize = System::Drawing::Size(1182, 673);
-			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->panelTitleBar);
+			this->Controls->Add(this->panelMenu);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"Form2";
 			this->Text = L"Matériel.Net - Accueil";
-			this->panel1->ResumeLayout(false);
-			this->panel2->ResumeLayout(false);
-			this->panel2->PerformLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->panelMenu->ResumeLayout(false);
+			this->panelLogo->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->btnHome))->EndInit();
 			this->ResumeLayout(false);
 
 		}
